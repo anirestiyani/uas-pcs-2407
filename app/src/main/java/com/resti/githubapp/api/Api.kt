51e6e0a0b6179abcad563a86d6da8e09d68af196
anirestiyani <anirestiyani064@gmail.com>
2021-01-20 09:36:54 +0700

    UAS PCS

--- app/src/main/java/com/resti/githubapp/api/Api.kt
+++ app/src/main/java/com/resti/githubapp/api/Api.kt
@@ -11,25 +11,25 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
+//    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
     fun getSearchUsers(
         @Query ("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
+//    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
     fun getUserDetail(
             @Path ("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
+//    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
     fun getFollowers(
             @Path ("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
+//    @Headers("Authorization: token 4fd807b448e3ff5e54d2ded3972e2719ec2742a1")
     fun getFollowing(
             @Path ("username") username: String
     ): Call<ArrayList<User>>
